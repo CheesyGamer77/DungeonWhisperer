ba0955794265879bb8100c722b71e6be8d81fb21
CheesyGamer77 <bartush.kyle.bd@gmail.com>
2021-08-03 09:43:42 -0400

    Change HelpCommand to ModOnlyHelpCommand

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 from cheesyutils.discord_bots import DiscordBot
+from cheesyutils.discord_bots.help_command import ModOnlyHelpCommand
+from cheesyutils.discord_bots.utils import get_discord_color
 from dislash import SlashClient
 
-bot = DiscordBot("$", members_intent=True, color="#1fcdff", status="idle", activity="in the Dungeon Depths", database="DungeonWhisperer.db")
+COLOR = get_discord_color("#1fcdff")
+
+bot = DiscordBot(";", members_intent=True, color=COLOR, status="idle", activity="in the Dungeon Depths", database="DungeonWhisperer.db", help_command=ModOnlyHelpCommand(COLOR))
 SlashClient(bot)
 
 bot.load_extension("cogs.community_pins")
